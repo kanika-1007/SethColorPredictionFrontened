2a66667404afebfa8f6c58b0e0ab7eb0cc7b2974
kanika-1007 <kpmittal505@gmail.com>
2025-01-23 23:13:07 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -27,8 +27,8 @@ const closeHistoryButton = document.getElementById('close-history-button');
 const BACKEND_URL = "https://seth-color-prediction.onrender.com";
 const scrollContainer = document.querySelector('.scroll-container');
 scrollContainer.addEventListener('scroll', () => {
-    if (scrollContainer.scrollLeft > 50) { // Limit scrolling to 300px
-        scrollContainer.scrollLeft = 50;
+    if (scrollContainer.scrollLeft > 70) { // Limit scrolling to 300px
+        scrollContainer.scrollLeft = 70;
     }
 });
 
