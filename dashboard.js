c661ca1bec4fe07ec8b54dd295600e30e2d26ebe
kanika-1007 <kpmittal505@gmail.com>
2025-01-26 15:32:58 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -218,7 +218,7 @@ async function fetchUserBalance() {
 }
 
 async function updateUserBalance(newBalance) {
-    console.log("new balance is"newbalance);
+    console.log("new balance is", newbalance);
     try {
         await fetch(`${BACKEND_URL}/api/dashboard/balance`, {
             method: 'POST',
